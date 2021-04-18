54b71329920a29469fec72e16793e788e3abbea7
mchmdarvan <muchamad.arvan27@gmail.com>
2021-04-18 16:10:44 +0700

    Convert HTML to Blade

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,16 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/debug-sentry', function () {
+    throw new Exception('My first Sentry error!');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
